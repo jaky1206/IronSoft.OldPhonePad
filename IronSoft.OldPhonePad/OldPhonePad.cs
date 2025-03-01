f71619b5e3945857312d5007fd174db66071946c
Sayed Rokibul Hossain <jaky1206@yandex.com>
2025-03-02 01:28:23 +0700

    initial changes

---
+++ IronSoft.OldPhonePad/OldPhonePad.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace IronSoft.OldPhonePad
+{
+    internal static class OldPhonePad
+    {
+        private static readonly Dictionary<char, char[]> KEY_MAPPING = new Dictionary<char, char[]>
+           {
+               { '1', new char[] { '&', '\'', '(', ')' } },
+               { '2', new char[] { 'A', 'B', 'C' } },
+               { '3', new char[] { 'D', 'E', 'F' } },
+               { '4', new char[] { 'G', 'H', 'I' } },
+               { '5', new char[] { 'J', 'K', 'L' } },
+               { '6', new char[] { 'M', 'N', 'O' } },
+               { '7', new char[] { 'P', 'Q', 'R', 'S' } },
+               { '8', new char[] { 'T', 'U', 'V' } },
+               { '9', new char[] { 'W', 'X', 'Y', 'Z' } },
+               { '0', new char[] { ' ' } },
+               { '*', new char[] { '\b' } },
+               { '#', new char[] { '\n' } },
+           };
+
+        public static bool IsValidKey(string key)
+        {
+            bool result = false;
+            if (key.Length == 1 && KEY_MAPPING.ContainsKey(key[0]))
+            {
+                result = true;
+            }
+            return result;
+        }
+
+        public static string GenerateOutput(List<KeyValuePair<string, int>> input)
+        {
+            StringBuilder output = new StringBuilder();
+
+            List<KeyValuePair<string, int>> inputMapping = new List<KeyValuePair<string, int>>();
+
+            foreach (var item in input)
+            {
+                char key = item.Key[0];
+                int duration = item.Value;
+                int index = 0;
+                if (KEY_MAPPING.ContainsKey(key))
+                {
+                    char[] chars = KEY_MAPPING[key];
+                    // if this is the first key add it with index zero
+                    if (inputMapping.Count == 0)
+                    {
+                        // Add the first key with index zero
+                        inputMapping.Add(new KeyValuePair<string, int>(chars[index].ToString(), index));
+                    }
+                    else
+                    {
+                        // if this is the same key as the previous one and duration is zero
+                        if (inputMapping[inputMapping.Count - 1].Key == chars[index].ToString() && duration == 0)
+                        {
+                            // increment the index
+                            index = (inputMapping[inputMapping.Count - 1].Value + 1) % chars.Length;
+                            // remove the old key
+                            inputMapping.RemoveAt(inputMapping.Count - 1);
+                            // add the key with the new index
+                            inputMapping.Add(new KeyValuePair<string, int>(chars[index].ToString(), index));
+                        }
+                        else
+                        {
+                            // if this is a different key
+                            // add the key with index zero
+                            inputMapping.Add(new KeyValuePair<string, int>(chars[index].ToString(), index));
+                        }
+                    }
+
+                }
+            }
+
+            foreach (var item in inputMapping)
+            {
+                // apply special handling for backspace and newline
+                if (item.Key == "\b")
+                {
+                    output.Remove(output.Length - 1, 1);
+                }
+                else
+                {
+                    output.Append(item.Key);
+                }
+                //output.Append(item.Key);
+            }
+
+            return output.ToString();
+        }
+    }
+}
---
+++ IronSoft.OldPhonePad/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using IronSoft.OldPhonePad;
+
+
+// Pause 1 second in order to type two characters on the same key.
+
+namespace IronSoft.OldPhonePad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Old feature phone keypad simulator.");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Allowed inputs: 0-9, *, #");
+            Console.WriteLine("Invalid inputs will be skipped.");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Key Mappings:");
+            Console.WriteLine("1 --> & ' ( ) ");
+            Console.WriteLine("2 --> A B C ");
+            Console.WriteLine("3 --> D E F ");
+            Console.WriteLine("4 --> G H I ");
+            Console.WriteLine("5 --> J K L ");
+            Console.WriteLine("6 --> M N O ");
+            Console.WriteLine("7 --> P Q R S ");
+            Console.WriteLine("8 --> T U V ");
+            Console.WriteLine("9 --> W X Y Z ");
+            Console.WriteLine("* --> Backspace ");
+            Console.WriteLine("0 --> Space ");
+            Console.WriteLine("# --> Send ");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Press enter to exit.");
+            Console.WriteLine("---------------------------------------------");
+
+            while (true)
+            {
+                List<KeyValuePair<string, int>> input = new List<KeyValuePair<string, int>>();
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                while (true)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+
+                    string keyPressed = key.KeyChar.ToString();
+                    if (IsValidKey(keyPressed))
+                    {
+                        if (keyPressed == "#")
+                        {
+                            Console.WriteLine("\nOutput: " + OldPhonePad.GenerateOutput(input));
+                            input.Clear();
+                        }
+                        else
+                        {
+                            input.Add(new KeyValuePair<string, int>(keyPressed, (int)stopwatch.Elapsed.TotalSeconds));
+                            stopwatch.Restart();
+                        }
+                    }
+                    else
+                    {
+                        Console.Beep();
+                        continue; // Skip adding the invalid key to the input
+                    }
+                }
+            }
+        }
+
+        private static bool IsValidKey(string key)
+        {
+            // read the list of invalid keys from the OldPhonePad class
+            return OldPhonePad.IsValidKey(key);
+        }
+    }
+}
